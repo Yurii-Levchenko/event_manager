0721173e4d59355ef6f2d28037414478ebf119bf
Yura Levchenko <levchenko.yurii024@gmail.com>
2024-11-11 11:34:04 +0200

    Laptop branch merged, custom model for authentication created, Main page template updated

--- meetups/models.py
+++ meetups/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from location_field.models.plain import PlainLocationField
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.utils import timezone
+from django.conf import settings
 
 from django.utils.translation import gettext_lazy as _
 from ckeditor_uploader.fields import RichTextUploadingField
@@ -9,7 +10,8 @@ from ckeditor_uploader.fields import RichTextUploadingField
 
 class Meetups(models.Model):
     title=models.CharField(max_length=200)
-    organizer=models.ForeignKey('Users', null=True, on_delete=models.SET_NULL)
+    # organizer=models.ForeignKey('Users', null=True, on_delete=models.SET_NULL)
+    organizer=models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
     slug=models.SlugField(unique=True)
     description=models.TextField()
     image=models.ImageField(upload_to='meetups_images/%Y/%m/%d/')
@@ -43,7 +45,8 @@ class Meetups(models.Model):
 class News(models.Model):
     title=models.CharField(max_length=200)
     slug=models.SlugField(unique=True, null=True, blank=True)
-    author=models.ForeignKey('Users', null=True, on_delete=models.SET_NULL)
+    # author=models.ForeignKey('Users', null=True, on_delete=models.SET_NULL)
+    author=models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
     image=models.ImageField(upload_to='news_images/%Y/%m/%d/', null=True, blank=True)
     description=models.TextField()
     content=RichTextUploadingField(null=True, blank=True)
@@ -75,7 +78,7 @@ class CustomUserManager(BaseUserManager):
         user = self.create_user(
             email,
             password=password,
-        )
+        ) 
         user.is_admin = True
         user.save(using=self._db)
         return user
@@ -84,13 +87,22 @@ class Users(AbstractBaseUser):
     name = models.CharField(max_length=200)
     surname = models.CharField(max_length=200)
     email = models.EmailField(unique=True)
-    image=models.ImageField(upload_to='user_images')
+    image=models.ImageField(upload_to='user_images', blank=True)
     is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
     # meetups = models.ManyToManyField(Meetups, blank=True)
     # news = models.ManyToManyField(News, blank=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name_plural = 'Users'
     
     def full_name(self):
         return f"{self.name} {self.surname}"
@@ -98,12 +110,21 @@ class Users(AbstractBaseUser):
     def __str__(self):
         return self.full_name()
     
-    objects = CustomUserManager()
+    def has_perm(self, perm, obj=None):
+        if self.is_superuser:
+            return True
+        
+        # Basic check for a specific object
+        # if obj is not None and hasattr(obj, 'owner'):
+        #     return obj.owner == self  # Allow if the user owns the object
+        
+        return perm in self.get_all_permissions()
+    
+    def has_module_perms(self, meetups):
+        if self.is_superuser:
+            return True
+        
+        return any(perm.startswith(meetups + '.') for perm in self.get_all_permissions())
+    
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
 
-
-
-    class Meta:
-        verbose_name_plural = 'Users'
